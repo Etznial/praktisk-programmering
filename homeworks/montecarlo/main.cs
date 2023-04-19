a45f7e646347dc228e616c775721b2e1a76bb8cd
MaltePiilGrau <malte.piil.grau@gmail.com>
2023-04-19 13:14:14 +0200

    hw monte carle trying to do B, some errors I do not understand

--- homeworks/montecarlo/main.cs
+++ homeworks/montecarlo/main.cs
@@ -11,16 +11,43 @@ class main{
 		var x=new vector(dim);
 		var rnd=new Random();
 		for(int i=0;i<N;i++){
-		for(int k=0;k<dim;k++)x[k]=a[k]+rnd.NextDouble()*(b[k]-a[k]);
-		double fx=f(x); sum+=fx; sum2+=fx*fx;
+			for(int k=0;k<dim;k++)x[k]=a[k]+rnd.NextDouble()*(b[k]-a[k]);
+			double fx=f(x); sum+=fx; sum2+=fx*fx;
 		}
 		double mean=sum/N, sigma=Sqrt(sum2/N-mean*mean);
 		var result=(mean*V,sigma*V/Sqrt(N));
 		return result;
 	}
 
-	//make 2D function
-	
+	public static (double,double) qrmc(Func<vector,double> f, vector a, vector b, int N){
+		int dim=a.size; double V=1; for(int i=0;i<dim;i++)V*=b[i]-a[i];
+		double sum=0,sum2=0;
+		var x=new vector(dim);
+		for(int i=0;i<N;i++){
+			/*here*/
+			double fx=f(x); sum+=fx; sum2+=fx*fx;
+		}
+		double mean=sum/N, sigma=Sqrt(sum2/N-mean*mean);
+		var result=(mean*V,sigma*V/Sqrt(N));
+		return result;
+	}
+
+	public static double corput(int n, int b){
+		double q=0;
+		double bk=1.0/b;
+		while(n>0){
+			q+=(n%b)*bk; 
+			n/=b; 
+			bk/b;
+		}
+		return q;
+	}
+
+	public static void halton(int n, int d, vector x){
+		int[] basis={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61};
+		int maxd=basis.size()/d;
+		assert(d<=maxd); for(int i=0; i<d; i++) x[i]=corput(n,basis[i]);
+	}
 	
 	public static void Main(){
 		Func<vector, double> cossin = (v) => Cos(v[0])+Sin(v[1]);
