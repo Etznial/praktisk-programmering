c8b2a1789451da95a24b26dea5ea95f7e784518b
MaltePiilGrau <malte.piil.grau@gmail.com>
2023-06-18 12:41:53 +0200

    hw A and B done for minimizer

--- homeworks/minimization/main.cs
+++ homeworks/minimization/main.cs
@@ -8,14 +8,16 @@ using System.Collections.Generic;
 
 class main{
 	public static int it=0;
+	public const double MEPS=2e-10;
 	public static int step=0;
 	public static vector qnewton(
 		Func<vector,double> f, 	/* objective function */
 		vector start, 		/* starting point */
-		double acc = 1e-3		/* accuracy goal, on exit |gradient| should be < acc */){
+		double acc = 1e-3	/* accuracy goal, on exit |gradient| should be < acc */){
 		
 		int n = start.size;
-		double lambda=1;
+		
+		double lambda;
 		double uy;
 		vector dx;
 		vector s; /* s=lambda*dx */
@@ -27,30 +29,30 @@ class main{
 		matrix B = new matrix(n,n);
 		B.set_identity();
 		for(int i=0; i<n; i++){
-			if(Abs(start[i]) < Pow(2,-26)) start[i] = Pow(2,-26);
+			if(Abs(start[i])<Pow(2,-26)) start[i]=Pow(2,-26);
 		}
-		while(grad.norm() > acc){
+		while(grad.norm()>acc){
 			step++;
 			dx=-B*grad;
+			lambda=1.0;
 			while(true){
 				s=lambda*dx; 
-				if(f(start+s)<f(start)){
+				if(f(start+s)<f(start)){ // accept the step and update B
 					start+=s;		
 					gradOld=grad;
 					grad=gradient(f,start);
 					y=grad-gradOld;
 					u=s-B*y;
 					uy=u.dot(y);
-					//check if ny is zero
-					if(Abs(uy) > 1e-6){
+					if(Abs(uy)>MEPS){
 						dB=matrix.outer(u,u)/uy;
 						B+=dB;
 					}
 					break;
 				}
 				lambda/=2;
-				if(lambda < 1.0/1024){
-					start+=s;
+				if(lambda < MEPS){
+					start+=lambda*dx;
 					grad=gradient(f,start);
 					B.set_identity();
 					break;
@@ -58,6 +60,13 @@ class main{
 			}
 		}
 		return start;
+	}
+	
+	public static vector fit(Func<vector, double> f, vector guess, genlist<double> xs, genlist<double> ys, genlist<double> errs, double acc){ // vectros will not work, have to use List<double> for ex yx and errs
+		Func<vector, double> dev = (x) => { // dev = deviation function
+			double sum = 0; for(int i=0; i<xs.size; i++) sum += Pow((f(new vector(xs[i], x[0], x[1], x[2])) - ys[i])/errs[i], 2); return sum;
+		};
+		return qnewton(dev, guess, acc);
 	}
 
 	public static vector gradient(Func<vector, double> f, vector x){
@@ -73,39 +82,7 @@ class main{
 		}
 		return grad;
 	}
-
-	public static vector qnewtonOld(
-		Func<vector,double> f, 	/* objective function */
-		vector start, 		/* starting point */
-		double acc = 2e-23		/* accuracy goal, on exit |gradient| should be < acc */){
-		int n = start.size;
-		/* Hessian matrix */
-		matrix H = new matrix(n,n);
-		vector x = start.copy();
-		vector x1 = start.copy();
-		vector x12 = start.copy(); /* x1 = xdx1, x12 = xdx1dx2 */
-		double dx1, dx2;
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				dx1 = Abs(x1[i])*Pow(2,-26);
-				dx2 = Abs(x12[j])*Pow(2,-26);
-				x1[i]+=dx1; x12[i]+=dx1; x12[j]+=dx2;/* fix x'erne */
-				WriteLine($"f(x12)={f(x12)}\tf(x1)={f(x1)}\tf(x)={f(x)}");
-				x.print("x  =");
-				x1.print("x1 =");
-				x12.print("x12=");
-				H[i,j]=((f(x12)-f(x))/dx1 - (f(x1)-f(x))/dx1)/dx2;
-				x1[i]-=dx1; x12[i]-=dx1; x12[j]-=dx2;/* fix x'erne */
-			}
-		}
-		H.print("Hessian matrix:");
-		
-
-
-		var temp = new vector(n);
-		return temp;
-
-	}
+	
 
 	static public double ros(vector v){
 		it++;
@@ -121,12 +98,15 @@ class main{
 	}
 
 
+
+
 	public static void Main(){
 		// A
 		WriteLine("==============================[A]==============================");
 		var start = new vector(2,2);
-		WriteLine($"minimum of the Rosenbrock's valley function, it took {step} steps, has one global minima at (1,1)");
-		qnewton(ros,start).print("awnser: "); step=0;
+		WriteLine($"minimum of the Rosenbrock's valley function");
+		qnewton(ros,start).print("awnser: ");
+		WriteLine($"should be approx (1,1) it took {step} steps\n"); step=0;
 		WriteLine();
 				
 		WriteLine("minimum of the Himmelblau's function, has four local minima at (3,2), (-2.8,3.1) (-3.8,-3.3) and (3.6,-1.8):\n");
@@ -140,11 +120,39 @@ class main{
 		WriteLine($"should be approx (3.6,-1.9) it took {step} steps\n"); step=0;
 		// B
 		WriteLine("==============================[B]==============================");
-		Func<vector, double> bw = (x) => x[1]/(Pow(x[0]-x[2], 2) + Pow(x[3],2)/4);
-		// initialise empty lists for energy, signal and error
-		List<double> energy = new List<double>();
-		List<double> signal = new List<double>();
-		List<double> error  = new List<double>();
+		Func<vector, double> bw = (x) => x[1]/(Pow(x[0]-x[2], 2) + Pow(x[3],2)/4); // E = 1, m = 2, gamma = 3, A = 4
+		var energy = new genlist<double>();
+		var signal = new genlist<double>();
+		var error  = new genlist<double>();
+		
+		string[] data = File.ReadAllLines("higgs.data");
+		string[] words;
+		var separators = new char[] {' ','\t'};
+		var options = StringSplitOptions.RemoveEmptyEntries;
+		for(int i = 0; i<data.Length; i++){
+			words = data[i].Split(separators, options);
+			energy.add(double.Parse(words[0]));
+			signal.add(double.Parse(words[1]));
+			error .add(double.Parse(words[2]));
+		}
+		WriteLine("energy\tsignal\terror");
+		for(int i=0;i<energy.size;i++){
+			WriteLine($"{energy[i]}\t{signal[i]}\t{error[i]}");
+		}
+
+		vector guess = new vector(1,1,1);
+		vector bw_params = fit(bw, guess, energy, signal, error,1e-4);
+		double A,m,gamma;A=bw_params[0];m=bw_params[1];gamma=bw_params[2];
+		WriteLine($"parameters for the Breit-Wigner function: A={A}\tm={m}\tgamma={gamma}");
+		
+		double bwData;
+		string toWrite="";
+		for(double E=100;E<160;E+=0.1){
+			bwData = bw(new vector(E,A,m,gamma));
+			toWrite+=$"{E}\t{bwData}\n";
+			
+		}
+		File.WriteAllText("bwfit.data",toWrite);
 
 		/*
 		qnewton(him,new vector(-2.8, 3.1)).print($"should be approx (-2.8,3.1):"); 
